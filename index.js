b000c68d4e7ecf651dd43071791709c7969b453b
Vesyosu <chrisbcvk@gmail.com>
2025-03-20 08:24:30 +0100

    Ammélioration des boutons, ajout de sons, changement du desing des pages HTML

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ function creer_sequence(liste, n){
 }
 
 function jouer_son(element){
-   var audio_1 = new Audio('sons/son_1.mp3');
+   var audio_1 = new Audio("sons/son_1.mp3");
    var audio_2 = new Audio("sons/son_2.mp3");
-;
+   var audio_3 = new Audio("sons/son_3.mp3");
+   var audio_4 = new Audio("sons/son_4.mp3");
+
    if(element==0){
       console.log("Jouer son 1");
       audio_1.play()
@@ -23,19 +25,23 @@ function jouer_son(element){
 
    else if(element==2){
       console.log("Jouer son 3")
-      alert("jaune")
+      audio_3.play()
       }
 
    else{
       console.log("Jouer son 4")
-      alert("vert")
+      audio_4.play()
       } 
 }
 
 function jouer_sequence(liste,n){
+   var silence = new Audio("sons/silence.mp3");//Déclaration d'une nouvelle instance audio qui contient une seconde de vide sonore 
+
    for(i=0;i<n;i++){
-      // CORRIGER L'UTILISATION DU DELAIS : LA FONCTON A L INTERIEUR NE SE JOUE QUUNE FOIS ????
-      setTimeout(jouer_son(liste[i]),1000);
+      // CORRIGER L'UTILISATION DU DELAIS : LA FONCTON A L'  éINTERIEUR NE SE JOUE QU'UNE FOIS ????
+      
+      jouer_son(liste[i])
+      silence.play()//Méthode en ajoutant manuellement un " silence" ne fonctionne pas 
    }
 }
 
@@ -47,3 +53,4 @@ function game_loop(){
    jouer_sequence(sequence,n)
 }
 
+game_loop()
